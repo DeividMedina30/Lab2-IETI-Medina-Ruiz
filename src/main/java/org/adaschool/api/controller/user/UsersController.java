a47ced40828afe58bedfa493a49d37a24a8f3c0b
Deivid Medina <deivid.medina@mail.escuelaing.edu.co>
2023-02-08 23:02:38 -0500

    Funciona pruebas get y put

--- src/main/java/org/adaschool/api/controller/user/UsersController.java
+++ src/main/java/org/adaschool/api/controller/user/UsersController.java
@@ -1,13 +1,18 @@
 package org.adaschool.api.controller.user;
 
+import org.adaschool.api.exception.UserNotFoundException;
 import org.adaschool.api.repository.user.User;
+import org.adaschool.api.repository.user.UserDto;
 import org.adaschool.api.service.user.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/users/")
@@ -29,19 +34,29 @@ public class UsersController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         //TODO implement this method
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(usersService.all());
     }
 
     @GetMapping("{id}")
     public ResponseEntity<User> findById(@PathVariable("id") String id) {
         //TODO implement this method
-        return ResponseEntity.ok(null);
+        try {
+            return new ResponseEntity<User>(usersService.findById(id).get(), HttpStatus.OK);
+        }catch (Exception e){
+            throw new UserNotFoundException(id);
+        }
     }
 
-    @PutMapping
-    public ResponseEntity<User> updateUser() {
-        //TODO implement this method
-        return ResponseEntity.ok(null);
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@RequestBody UserDto userDto, @PathVariable("id") String id) {
+        User user = new User(userDto);
+        Optional<User> usuarioExistente = usersService.findById(id);
+        if (!usuarioExistente.isEmpty()) {
+            usersService.update(user, id);
+            usersService.save(usuarioExistente.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        throw new UserNotFoundException(id);
     }
 
     @DeleteMapping
